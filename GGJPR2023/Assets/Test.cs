a9242857930fcfd34ad083fd304e11cc562fcc22
marrero171 <50031007+marrero171@users.noreply.github.com>
2023-01-30 20:12:37 -0400

    Added ExtEvents

---
+++ GGJPR2023/Assets/Test.cs
@@ -0,0 +1,20 @@
+using ExtEvents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private ExtEvent _testEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
